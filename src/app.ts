dc5ca6956fd546ec038a4bd867655662a1b3490d
Dan <danci1159@gmail.com>
2023-12-14 22:15:47 +0100

    Fix typo in console log message

--- src/app.ts
+++ src/app.ts
@@ -11,5 +11,5 @@ setupRoutes(app);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`[server]: Server is rasdasunning at http://localhost:${PORT}`);
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
