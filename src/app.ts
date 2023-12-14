b7b2b17bcc90ac8858d0a8cccae5d679c816012a
Dan <danci1159@gmail.com>
2023-12-14 21:20:12 +0100

    Add express-route-grouping library for API route grouping

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import dotenv from "dotenv";
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, Router } from "express";
+import RouteGroup from "express-route-grouping";
 
 dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
+const root = new RouteGroup("/", app);
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
+});
+
+root.group("/api/v1", (router: Router) => {
+  router.get("/test", (req: Request, res: Response) => {
+    res.send("Hello World");
+  });
 });
 
 app.listen(PORT, () => {
