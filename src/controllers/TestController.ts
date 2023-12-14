746c35275e4d1b9c4d6d4fad365469e3ce9638a9
Dan <danci1159@gmail.com>
2023-12-14 21:59:42 +0100

    asdasda

--- src/controllers/TestController.ts
+++ src/controllers/TestController.ts
@@ -1,31 +1,25 @@
 import { Request, Response } from "express";
 class TestController {
-  // GET: /products
   index = (req: Request, res: Response) => {
     res.send("Test Controller Index");
   };
 
-  // GET: /products/:productId
   find = (req: Request, res: Response) => {
     res.send("Test Controller Find");
   };
 
-  // POST: /products
   create = (req: Request, res: Response) => {
     res.send("Test Controller Create");
   };
 
-  // PUT: /products/:productId
   update = (req: Request, res: Response) => {
     res.send("Test Controller Update");
   };
 
-  // PATCH: /products/:productId
   patch = (req: Request, res: Response) => {
     res.send("Test Controller Patch");
   };
 
-  // DELETE: /products/:productId
   delete = (req: Request, res: Response) => {
     res.send("Test Controller Delete");
   };
